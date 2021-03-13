dbea0a9ffc202b3e4167ae849c459c7266969a37
Scott Sauyet <scott@sauyet.com>
2021-03-13 16:21:36 -0500

    Theme restructuring

--- themes.js
+++ themes.js
@@ -1,4 +1,4 @@
-const themes = {
+const themes = ((defaultTheme = 'A', colors = {
   'A': {
         'neutral': '#d7e3e5',
         'primary': '#edd8c8',
@@ -43,7 +43,27 @@ const themes = {
     'text-tertiary': '#98b8c6',
     'text-highlight': '#666666',
  },
-}
- 
-
-const defaultTheme = 'A'
+}) => ({
+    colors,
+    icons: Object .fromEntries (
+        Object .entries (colors) .map (
+            ([k, {neutral, primary, secondary, tertiary, 'text-tertiary': tt}]) => 
+      [k, 
+`<svg viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0" y="0" rx="30" ry="30" width="300" height="200"
+      style="fill: ${neutral};"/>
+    <rect x="20" y="80" rx="15" ry="15" width="160" height="100"
+      style="fill: ${primary}; stroke-width: 2; stroke: ${tt};"/>
+    <rect x="200" y="80" rx="15" ry="15" width="80" height="100"
+      style="fill: ${secondary}; stroke-width: 2; stroke: ${tt};"/>
+    <rect x="20" y="20" rx="15" ry="15" width="260" height="40"
+      style="fill: ${tertiary}; stroke-width: 2; stroke: ${tt};"/>
+</svg>`])),
+    change: (name) => {
+        const style = document.querySelector(':root').style
+        Object.entries(colors[name] || {}) .forEach (
+          ([key, value]) => style .setProperty(`--${key}`, value)
+        )  
+    },
+    defaultTheme
+}))()
